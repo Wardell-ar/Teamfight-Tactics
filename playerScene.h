04be40aee8d5003060d6938bf0045ff6bd252b1f
lycmoons <2915017352@qq.com>
2023-12-23 16:49:02 +0800

    new

--- playerScene.h
+++ playerScene.h
@@ -9,13 +9,8 @@ USING_NS_CC;
 extern Vector<Hero*> allMyHeroes;//我方所有英雄
 extern Vector<Hero*> allEnemyHeroes;//敌方所有英雄
 extern int fight;
-/*
-  put a chessboard photo(include preparation seat and chessboard)
-  set a menu (option) to exit the game or stop the music
 
-  func:
-  the move of the player role
-*/
+
 class playerScene : public Scene
 {
 public:
@@ -32,48 +27,22 @@ public:
     void startTimer() {
         schedule(CC_SCHEDULE_SELECTOR(playerScene::attack), 2.0f);  // 每隔1秒执行一次checkCondition函数
     }
-
     // 攻击方法
     void attack(float dt)
     {
         fight = 1;
-        // 根据阵营将所有处于战斗状态的英雄分别存储到两个堆里面
-        std::vector<Hero*> myHeroes;
-        std::vector<Hero*> enemyHeroes;
-
-        // 遍历所有战斗英雄
-
-        // 你需要根据实际情况修改
-        for (auto hero : allMyHeroes)
-        {
-            if (hero->isInBoard() && fight == 1)
-            {
-                myHeroes.push_back(hero);
-
-            }
-        }
-        for (auto hero : allEnemyHeroes)
-        {
-            if (hero->isInBoard() && fight == 1)
-            {
-                enemyHeroes.push_back(hero);
-            }
-        }
-
-
-
 
         bool attacked = 0;
 
         //我方攻击动作
-        for (auto myHero : myHeroes)
+        for (auto myHero : allMyHeroes)
         {
-            if (!myHero->isDead()) {
-                Hero* target = enemyHeroes.front();
+            if (!myHero->isDead()&& myHero->isInBoard()) {
+                Hero* target;
                 bool targetFind = 0;
                 long long nearst_distance = 99999999999;
-                for (auto enemyHero : enemyHeroes) {
-                    if (!enemyHero->isDead()) {
+                for (auto enemyHero : allEnemyHeroes) {
+                    if (!enemyHero->isDead() && enemyHero->isInBoard()) {
                         targetFind = 1;
                         long long x_distance = myHero->getPosition().x - enemyHero->getPosition().x;
                         long long y_distance = myHero->getPosition().y - enemyHero->getPosition().y;
@@ -88,18 +57,17 @@ public:
                     myHero->attack(target);
                     attacked = 1;
                 }
-
             }
         }
         //敌方攻击动作
-        for (auto enemyHero : enemyHeroes)
+        for (auto enemyHero : allEnemyHeroes)
         {
-            if (!enemyHero->isDead()) {
-                Hero* target = myHeroes.front();
+            if (!enemyHero->isDead()&&enemyHero->isInBoard()) {
+                Hero* target;
                 bool targetFind = 0;
                 long long nearst_distance = 99999999999;
-                for (auto myHero : myHeroes) {
-                    if (!myHero->isDead()) {
+                for (auto myHero : allMyHeroes) {
+                    if (!myHero->isDead()&&myHero->isInBoard()) {
                         targetFind = 1;
                         long long x_distance = myHero->getPosition().x - enemyHero->getPosition().x;
                         long long y_distance = myHero->getPosition().y - enemyHero->getPosition().y;
